4aca0ce60209071cbcd79b352ae8071981953b2b
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-12 13:46:47 -0400

    Added env file handling

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+// read from a .env located at the root of the project
+// and merge the key/value pairs into process.env
 const server = require('./api/server.js');
 
 // make the port be assigned by the Server
@@ -6,3 +9,4 @@ const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+ 
